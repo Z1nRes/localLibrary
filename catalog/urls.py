26345d17b4a56845eb8ed37810329f117f1f916e
Z1nRes <tv.noob@bk.ru>
2022-09-15 10:37:58 +0700

    step6

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
-from django.conf.urls import url
+
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^books/$', views.BookListView.as_view(), name='books'),
-    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    re_path(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+    re_path(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
